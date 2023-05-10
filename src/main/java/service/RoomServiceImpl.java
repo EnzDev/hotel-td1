0e8d9b4057c87655ce3a75ee9a37877834dcb8f7
Enzo Mallard <enzo@mallard.ml>
2023-05-10 10:49:47 +0200

    clean: Create room price rule

--- src/main/java/service/RoomServiceImpl.java
+++ src/main/java/service/RoomServiceImpl.java
@@ -1,21 +1,13 @@
 package service;
 
+import administration.business.rules.RoomPriceDependingOnFloor;
 import dao.RoomDao;
 import administration.business.entity.Room;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public class RoomServiceImpl implements RoomService {
-    private static final double MAX_ROOM_PRICE = 200;
-    public static final Map<Integer, Double> FLOOR_RATES = new HashMap<Integer, Double> () {{
-        put(0, 1.0);
-        put(1, 1.07);
-        put(2, 1.22);
-        put(3, 1.33);
-    }};
 
 
     private final RoomDao dao;
@@ -36,20 +28,11 @@ public class RoomServiceImpl implements RoomService {
                 .stream()
                 .map(room -> {
                     // Compute new price
-                    double newPrice = getRoomPrice(groundFloorPrice, room);
+                    double newPrice = RoomPriceDependingOnFloor.getRoomPrice(groundFloorPrice, room.getFloor());
 
                     return new Room(room.getFloor(), room.getRoomNumber(), newPrice);
                 }).collect(Collectors.toList());
 
         dao.setRooms(roomsWithNewPrice);
-    }
-
-    private double getRoomPrice(double basePrice, Room room) {
-        Double rate = FLOOR_RATES.get(room.getFloor());
-        if (rate == null) {
-            throw new IllegalStateException("Room has an invalid floor");
-        }
-
-        return Math.min(basePrice * rate, MAX_ROOM_PRICE);
     }
 }
